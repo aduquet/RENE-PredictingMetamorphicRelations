e281ed7d8a420be46dd0e319726e8c77f1948f15
aduquet <duquet@ut.ee>
2021-10-21 11:47:47 +0200

    Python script to compute kernels

--- Phase_II-DataPreparation/myKernels.py
+++ Phase_II-DataPreparation/myKernels.py
@@ -2,13 +2,13 @@ import networkx as nx
 import numpy as np
 from scipy.sparse.linalg import lsqr
 from scipy.sparse import lil_matrix, kron, identity
+import itertools
 
 def get_norm(adj_mat):
     norm = adj_mat.sum(axis=0)
     norm[norm == 0] = 1
 
     return adj_mat / norm
-
 
 def RWK(X, Y):
     step = 10
@@ -33,4 +33,70 @@ def RWK(X, Y):
     rwk_norm = rwk / np.sqrt(rwk_1 * rwk_2)
 
     return rwk_norm
+
+def nodeLabelExactMatch(nodeMap,labels):
+	check=True
+	for i in nodeMap:
+		if labels[i]!=labels[nodeMap[i]]:
+			check=False
+	return check
+
+def getSubgraphs(graph,size):
+
+	nodes = graph.nodes()
+	labels = nx.nodes(graph) 
+	combs = itertools.combinations(nodes,size)
+	connectedSubgs = []
+	subgs = {}
+
+	for i in combs:
+		subg=graph.subgraph(i)
+		if nx.is_weakly_connected(subg):
+			connectedSubgs.append(subg)   
+
+	for i in connectedSubgs:
+		cnt=1	
+		for j in connectedSubgs:
+			#print j
+			if i!=j:
+				DiGM = nx.isomorphism.DiGraphMatcher(i,j)
+				if DiGM.is_isomorphic():
+					nodeMap=DiGM.mapping
+					if nodeLabelExactMatch(nodeMap, labels):
+						cnt+=1
+						connectedSubgs.remove(j)
+		
+		subgs[i]=float(cnt)/float(len(connectedSubgs)) 
+	
+	return subgs
+
+
+#calculates the k_graph(G,G') for two graphs 
+def calcGraphletKernelVal(g1Graphlets,g1NodeLabels,g2Graphlets,g2NodeLabels):
+
+	k_graph=0
+	for i in g1Graphlets:
+		for j in g2Graphlets:
+			k_graph += calcSubgraphKernelVal(i,g1NodeLabels,j,g2NodeLabels)*g1Graphlets[i]*g2Graphlets[j]
+	return k_graph			 
+
+def getNodeKernelValExactEqual(l1,l2):
+
+	if(l1 == l2): 
+		return 1
+	else:
+		return 0
+
+#calculate the kernel value k_subgraph(subg1,subg2) for two subgraphs 
+def calcSubgraphKernelVal(subg1,subg1NodeLabels,subg2,subg2NodeLabels):
+
+	DiGM = nx.isomorphism.DiGraphMatcher(subg1,subg2)
+	if DiGM.is_isomorphic():
+		nodeMap=DiGM.mapping
+		k_subgraph=1
+		for n in nodeMap:
+			k_subgraph=k_subgraph*getNodeKernelValExactEqual(subg1NodeLabels[n],subg2NodeLabels[n])
+	else:
+		k_subgraph=0
+	return k_subgraph
 
