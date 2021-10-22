99799ce086d23b808a7d667fe53b0c9cad31f0ca
aduquet <duquet@ut.ee>
2021-10-22 12:10:52 +0200

    Feature extractor GK

--- Phase_II-DataPreparation/featureExtractor-GK.py
+++ Phase_II-DataPreparation/featureExtractor-GK.py
@@ -1,19 +1,90 @@
 import os
-import sys
+import pathlib
+import glob as gl
+import numpy as np
+from numpy.testing._private.utils import print_assert_equal
+import pandas as pd
 import networkx as nx
-
 
 from myKernels import *
 
+def create_df(matrixK, dot_names):
+
+    columnsName = []
+
+    for i in range(np.shape(matrixK)[0]):
+        name = 'FM_' + str(i+1)
+        columnsName.append(name)
+    
+    index = list(dot_names['dot_name'])
+
+    df_rwk = pd.DataFrame(matrixK, columns= columnsName, index= index)
+    
+    return df_rwk 
+
+def get_names(paths):
+    
+    dot_name = []
+    dot_path_aux = []
+    for dot in paths:
+        name = dot.split('\\')[-1]
+        name = name.split('.dot')[0]
+        dot_name.append(name)
+        dot_path_aux.append(dot)
+    df_main['dot_name'] = dot_name
+    df_main['dot_path'] = dot_path_aux
+    return df_main
+
+def read_dot(names_paths_df):
+    paths = names_paths_df['dot_path']
+    cfg_final = []
+    for i in paths:
+        cfg = nx.drawing.nx_pydot.read_dot(i)
+        cfg_final.append(cfg)
+    # print(len(cfg_final))
+    return cfg_final
+
+def saveFile(df, path, output):
+
+    if output.find('.') != -1:
+        output = output.split('.')[0]
+    
+    finalPath = path + '\\' + output + '.csv'
+    df.to_csv(finalPath)
+    print('\n DONE! File saved in: \n ', finalPath )
 
 
-g1=nx.drawing.nx_pydot.read_dot('C:/Users/duquet/Documents/GitHub/RENE-PredictingMetamorphicRelations/Java/Java dot files - from Kanewala/sum.dot')
-g2=nx.drawing.nx_pydot.read_dot('C:/Users/duquet/Documents/GitHub/RENE-PredictingMetamorphicRelations/Java/Java dot files - from Kanewala/sum.dot')
-g1NodeLabels=nx.nodes(g1)
-g1Graphlets = getSubgraphs(g1, 1)
-print(len(g1NodeLabels))
-g2NodeLabels = nx.nodes(g2)
-g2Graphlets = getSubgraphs(g2, 1)
-val=calcGraphletKernelVal(g1Graphlets,g1NodeLabels,g2Graphlets,g2NodeLabels)
-print(val)
-# create_graphlet_kernel_matrix(sys.argv[1],sys.argv[2],int(sys.argv[3]))
+if __name__ == '__main__':
+    import click
+    global df_main
+    df_main = pd.DataFrame()
+
+
+    @click.command()
+    @click.option('-i', '--csv input file', 'dot_input', help= 'Path to the dot files')
+    @click.option('-o', '--csv output file', 'output_file', help= 'Name of the csv output file')
+
+    def main(dot_input, output_file):
+
+        here_iam = str(pathlib.Path().absolute())
+        resultsPath = here_iam + '\\GK_Features'
+        
+        if not os.path.exists(resultsPath):
+            os.mkdir(resultsPath)
+
+        dot_path = gl.glob(dot_input)
+        names_paths_df = get_names(dot_path)
+        cfg_main = np. asarray(read_dot(names_paths_df))
+
+        matrixK = np.zeros([len(cfg_main), len(cfg_main)])
+
+        for i in range(0, np.shape(matrixK)[0]):
+            for j in range(0, np.shape(matrixK)[1]):
+                matrixK[i, j] = GK(cfg_main[i], cfg_main[j])
+            print(matrixK)
+            df_rwk = create_df(matrixK, df_main)
+            saveFile(df_rwk, resultsPath, output_file)
+        
+        print(output_file)
+    
+main()
