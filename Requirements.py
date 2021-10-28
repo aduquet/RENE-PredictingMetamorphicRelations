455b9bc4863c05e0d6406e0a2fca5a29b4994eb9
aduquet <duquet@ut.ee>
2021-10-28 14:39:07 +0200

    Requirements

---
+++ Requirements.py
@@ -0,0 +1,37 @@
+import subprocess
+
+# Pip upgrade
+
+# subprocess.run('pip install --upgrade pip')
+
+# Click library
+subprocess.run('pip install click')
+
+# os sys
+# subprocess.run('pip install os-sys')
+
+# path lib
+subprocess.run('pip install pathlib')
+
+# numpy lib
+subprocess.run('pip install numpy')
+
+# glob lib
+# subprocess.run('pip install glob')
+
+# pandas lib
+subprocess.run('pip install pandas')
+
+# networkx
+subprocess.run('pip install networkx')
+
+# nx_pydot
+subprocess.run('pip install nx_pydot')
+
+# scipy lib
+subprocess.run('pip install scipy')
+
+# pydot lib
+subprocess.run('pip install pydot')
+
+
